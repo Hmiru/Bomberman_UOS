ec67cb9826228a0ec0474c038aa9576be4f62c51
unknown <wjkim9911@gmail.com>
2025-12-11 16:14:45 +0900

    update

--- agent_code/rule_based_agent/callbacks.py
+++ agent_code/rule_based_agent/callbacks.py
@@ -11,7 +11,7 @@ def look_for_targets(free_space, start, targets, logger=None):
 
     Performs a breadth-first search of the reachable free tiles until a target is encountered.
     If no target can be reached, the path that takes the agent closest to any target is chosen.
-
+s
     Args:
         free_space: Boolean numpy array. True for free tiles and False for obstacles.
         start: the coordinate from which to begin the search.
